9ee55dc18d86bda88d3e130b23f6f05d9e2c2232
Adrian1706 <adrianjbuenop@gmail.com>
2023-08-16 15:14:26 -0500

    feat: :sparkles: Metodo post en bodegas
    Se realiza el metodo post en bodegas para poder enviar datos a la tablas bodegas

--- routers/bodegas.js
+++ routers/bodegas.js
@@ -1,7 +1,6 @@
 import { conexion } from "../db/atlas.js";
 import { Router } from "express";
 
-
 const appBodegas = Router();
 
 let db = await conexion();
@@ -13,6 +12,17 @@ appBodegas.get("/", async(req, res)=>{
      let Bodegas = db.collection('bodegas');
      let result = await Bodegas.find({}).sort({nombre:1}).toArray();
      res.send(result);
-})
+});
+
+appBodegas.post("/", async(req, res)=>{
+     let result;
+     try {
+         result = await Bodegas.insertOne(req.body);
+         res.status(201).send(result);
+     } catch (error) {
+         console.log(error.errInfo.details.schemaRulesNotSatisfied[0]);
+         res.send();
+     }
+});
 
 export default appBodegas;
